1da72ef25e6963c51685c8de84d6b81f65b482dd
robertBrem <brem.rob@gmail.com>
2015-04-25 13:23:43 +0200

    initial commit

--- app/app.js
+++ app/app.js
@@ -1,12 +1,102 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+var myApp = angular.module('myApp', [
+    'ngRoute',
+    'myApp.entries'
+]);
+
+myApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .otherwise({redirectTo: '/entries'});
 }]);
+
+myApp.config(function ($provide) {
+    var directives = [
+        "alert"
+    ];
+    for (var i = 0; i < directives.length; i++) {
+        $provide.decorator(directives[i] + "Directive", function ($delegate) {
+            $delegate[0].templateUrl = "bower_components/angular-ui-bootstrap/" + $delegate[0].templateUrl;
+            return $delegate;
+        });
+    }
+});
+
+var auth = {};
+
+var logout = function () {
+    console.log('*** LOGOUT');
+    auth.loggedIn = false;
+    auth.authz = null;
+    window.location = auth.logoutUrl;
+};
+
+angular.element(document).ready(function () {
+
+    console.log("*** here");
+    var keycloakAuth = new Keycloak('keycloak.json');
+    auth.loggedIn = false;
+
+    keycloakAuth.init({onLoad: 'login-required'}).success(function () {
+        console.log('here login');
+        auth.loggedIn = true;
+        auth.authz = keycloakAuth;
+        auth.logoutUrl = keycloakAuth.authServerUrl + "/realms/" + keycloakAuth.realm + "/tokens/logout?redirect_uri=http://localhost:8000/#/admin";
+        myApp.factory('Auth', function () {
+            return auth;
+        });
+        angular.bootstrap(document, ["myApp"]);
+    }).error(function () {
+        alert("failed to login");
+    });
+
+});
+
+myApp.factory('authInterceptor', function ($q, Auth) {
+    return {
+        request: function (config) {
+            var deferred = $q.defer();
+            if (Auth.authz.token) {
+                Auth.authz.updateToken(5).success(function () {
+                    config.headers = config.headers || {};
+                    config.headers.Authorization = 'Bearer ' + Auth.authz.token;
+
+                    deferred.resolve(config);
+                }).error(function () {
+                    deferred.reject('Failed to refresh token');
+                });
+            }
+            return deferred.promise;
+        }
+    };
+});
+
+myApp.config(function ($httpProvider) {
+    $httpProvider.responseInterceptors.push('errorInterceptor');
+    $httpProvider.interceptors.push('authInterceptor');
+
+});
+
+myApp.factory('errorInterceptor', function ($q) {
+    return function (promise) {
+        return promise.then(function (response) {
+            return response;
+        }, function (response) {
+            if (response.status == 401) {
+                console.log('session timeout?');
+                logout();
+            } else if (response.status == 403) {
+                alert("Forbidden");
+            } else if (response.status == 404) {
+                alert("Not found");
+            } else if (response.status) {
+                if (response.data && response.data.errorMessage) {
+                    alert(response.data.errorMessage);
+                } else {
+                    alert("An unexpected server error has occurred");
+                }
+            }
+            return $q.reject(response);
+        });
+    };
+});
---
+++ app/views/entries/entries.js
@@ -0,0 +1,89 @@
+'use strict';
+
+var entriesApp = angular.module('myApp.entries', ['ngRoute', 'ui.bootstrap.alert', 'ngSanitize', 'configuration']);
+
+entriesApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/entries/:id', {
+        templateUrl: 'views/entries/entries.html',
+        controller: 'EntriesCtrl'
+    }).when('/entries', {
+        templateUrl: 'views/entries/entries.html',
+        controller: 'EntriesCtrl'
+    });
+}]);
+
+entriesApp.controller('EntriesCtrl', function ($scope, $http, $routeParams, $location, ENTRY_URL) {
+
+    $scope.alerts = [];
+
+    $scope.closeAlert = function (index) {
+        $scope.alerts.splice(index, 1);
+    };
+
+    $http.get(ENTRY_URL).then(function (response) {
+        $scope.entries = response.data;
+    });
+
+    if ($routeParams.id) {
+        $http.get(ENTRY_URL + $routeParams.id).
+            success(function (data) {
+                $scope.current = data;
+            }).
+            error(function (data, status, headers, config) {
+                // error
+            });
+    } else {
+        $scope.current = {};
+        $scope.current.creationDate = new Date();
+    }
+
+    $scope.toHTML = function (content) {
+        if (!content) {
+            return '';
+        }
+        return content.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    };
+
+    $scope.create = function (entry) {
+        entry.content = $scope.toHTML(entry.content);
+        entry.creationDate = undefined;
+
+        $http.post(ENTRY_URL,
+            entry)
+            .then(function (response) {
+                $scope.created = response.data;
+                $scope.alerts.push({type: 'success', msg: 'Juhuuu! ' + entry.title + ' wurde erfolgreich erstellt.'});
+            });
+    };
+
+    $scope.update = function (entry) {
+        entry.content = $scope.toHTML(entry.content);
+
+        $http.put(ENTRY_URL + $routeParams.id,
+            entry)
+            .then(function (response) {
+                $scope.created = response.data;
+                $scope.alerts.push({
+                    type: 'success',
+                    msg: 'Juhuuu! ' + entry.title + ' wurde erfolgreich aktualisiert.'
+                });
+            });
+    };
+
+    $scope.delete = function (id) {
+        $http.delete(ENTRY_URL + id)
+            .then(function (response) {
+                $scope.created = response.data;
+                $scope.alerts.push({
+                    type: 'success',
+                    msg: 'Juhuuu! ' + $scope.current.title + ' wurde erfolgreich gelöscht.'
+                });
+            });
+    };
+
+    $scope.clear = function () {
+        $location.path('/entries');
+        $scope.current = {};
+        $scope.current.creationDate = new Date();
+    };
+});
